570f3e59fe37089d1f1c4611accabde06c8eaaa5
离子 <liyang20097@163.com>
2017-01-13 11:21:08 +0800

    cooking demo 的初始化

---
+++ src/pages/home/app.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>首页</h1>
+        <p>A vue project. indexasdfjsasdfasdf2</p>
+        <button @click="ccc">vvvvvvv</button>
+        <button @click="showToast">showToastd</button>
+        <mt-button type="primary" @click="showMsgbox">showMsgbox</mt-button>
+        <mt-button type="primary" @click="sheetVisible = true">
+            选择操作
+        </mt-button>
+    </div>
+</template>
+<script>
+import {
+    Toast,
+    MessageBox
+} from 'mint-ui';
+
+export default {
+    name: 'app',
+    methods: {
+        ccc: function() {
+            console.log("cccccc")
+
+        },
+        showToast() {
+            Toast('这是一个 Toast');
+        },
+
+        showMsgbox() {
+            MessageBox('提示', '这是一个 Message Box');
+        }
+
+    }
+};
+</script>
+<style>
+html {
+    text-align: center;
+}
+</style>
